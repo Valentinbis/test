fbea9bed448893f58fd7a40bbc0f1c5d51909ffe
Valentin Bissay <valentin.bissay@gmail.com>
2020-10-29 15:30:54 +0100

    Création fichiers twing

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task", name="task")
+     * @Route("/tasks/listing/", name="tasks_listing")
      */
-    public function index(): Response
+    public function taskListing(): Response
     {
+        // On va chercher par doctrine le repository de nos Task
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // dans ce repository nous récupérons toutes les données
+        $tasks = $repository->findAll();
+
+        // affichage des données dans le var_dumper
+        dd($tasks);
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
     }
 }
